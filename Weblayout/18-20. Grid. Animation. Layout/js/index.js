59bdf69ca6e31a40d607724933ca36076f7b2b01
Nikita <ch4shkin.nikita@gmail.com>
2022-12-22 00:44:54 +0500

    18-20. Grid. Animation. Layout add

---
+++ Weblayout/18-20. Grid. Animation. Layout/js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#burger').addEventListener('click', function () {
+    document.querySelector('#menu').classList.add('is-active')
+  });
+
+  document.querySelector('#burger--close').addEventListener('click', function () {
+    document.querySelector('#menu').classList.remove('is-active')
+  });
+
+});
